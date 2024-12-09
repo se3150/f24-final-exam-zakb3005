251b611e9b5f6758a4c2db5dd9aa222136b0b6fe
zakb3005 <zakb3005@gmail.com>
2024-12-09 14:53:01 -0700

    test

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -5,8 +5,11 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-@then('I expect that message is encoded as "{msg}"')
+@then('I expect that message is encoded/decoded as "{msg}"')
 def step_impl(context, msg):
     decoded = context.driver.find_element(By.ID, "decoded_message")
     p = decoded.find_element(By.CSS_SELECTOR,"p")
+    
+    #basically just trying to find the paragraph element in decoded_message and make sure
+    #the message is what's expected.
     assert p == msg
