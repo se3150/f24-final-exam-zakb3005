1e6b17c532674d2b4dab730ad8a2d50cceff842d
zakb3005 <zakb3005@gmail.com>
2024-12-09 14:48:35 -0700

    test

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,10 +1,11 @@
 from behave import given, when, then
+from behave_webdriver.steps import *
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-@then('I expect that message is decoded as "{msg}"')
+@then('I expect that message is encoded as "{msg}"')
 def step_impl(context, msg):
     decoded = context.driver.find_element(By.ID, "decoded_message")
     p = decoded.find_element(By.CSS_SELECTOR,"p")
